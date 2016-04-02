9ba332661adb05e148befb115187767fe1a9c6ee
Cris Ward <cris@duodesign.co.uk>
2016-04-02 01:02:52 +0100

    changed back to phantom for travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function (config) {
   'use strict';
   var saucelabsBrowsers = require('./browsers').browsers
-  var browsers = ['Chrome']
+  var browsers = ['PhantomJS']
   if (process.env.SAUCE_USERNAME) {
     browsers = Object.keys(saucelabsBrowsers)
   }
